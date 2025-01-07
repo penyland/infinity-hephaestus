e71ed5d2ab8777c987c94c41531f4b54f7c4cf51
Peter Nylander <peter.nylander@gmail.com>
2025-01-07 09:19:37 +0100

    Add Azure provisioning support and update project files
    - Introduced `DevelopmentInfrastructure` class for Key Vault configuration.
    - Updated `.editorconfig` for code analysis rules.
    - Targeted .NET 9.0 in `Odin.Infrastructure.csproj` and added Azure provisioning packages.
    - Updated solution file to include new project configurations.
    - Modified `Program.cs` to demonstrate new functionality.

---
+++ src/Odin.Infrastructure/DevelopmentInfrastructure.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Provisioning.KeyVault;
+
+namespace Odin.Infrastructure;
+
+public class DevelopmentInfrastructure : Azure.Provisioning.Infrastructure
+{
+    public DevelopmentInfrastructure() : base() { }
+
+    public void AddKeyVault()
+    {
+        var keyVault = new KeyVaultService("odin_kv");
+        keyVault.Properties = new()
+        {
+            Sku = new()
+            {
+                Family = KeyVaultSkuFamily.A,
+                Name = KeyVaultSkuName.Standard,
+            },
+            EnableSoftDelete = true,
+            EnablePurgeProtection = false,
+            EnableRbacAuthorization = true,
+        };
+
+        Add(keyVault);
+    }
+}
---
+++ src/Odin.Infrastructure/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Odin.Infrastructure;
+
+var infrastructure = new DevelopmentInfrastructure();
+
+infrastructure.AddKeyVault();
+
+var provisioningPlan = infrastructure.Build();
+provisioningPlan.Compile();
+provisioningPlan.Save(".");
